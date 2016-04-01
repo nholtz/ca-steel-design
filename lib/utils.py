6115b98ae684ae15ac741d02fcc0ddfa76acc7a0
Neal Holtz <neal.holtz@carleton.ca>
2016-03-31 22:02:15 -0400

    add showImage() to utils

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,9 @@ import re
 import string
 import math
 import sys
+import os
+import os.path
+import imghdr
 import inspect
 import numpy as np
 
@@ -39,6 +42,54 @@ def SVG(filename,**kwargs):
         svgdata = inf.read()
     outdata = _formatter.vformat(svgdata,[],kwargs)
     return display.SVG(data=outdata)
+
+def _test_svg(bstream,fileobj):
+    if fileobj:
+        fileobj.seek(0L)
+        data = fileobj.read(6)
+    else:
+        data = bstream.read(6)
+    if data == '<?xml ':
+        return 'svg'
+    return None
+imghdr.tests.append(_test_svg)
+
+EXTS = ['svg','png','jpeg','jpg']
+
+def showImage(basename,rescan=False):
+    """Display an image whose basename is 'basename'.  If an image
+    cannot be found, the user is asked to scan an image, which is
+    then installed.  Extensions '.jpg', '.png', '.svg' are tried in
+    that order.  If rescan is True, scanning a new image is forced."""
+    def _display(ifile):
+        itype = imghdr.what(ifile)
+        img = None
+        if itype in ['jpeg','png']:
+            img = display.Image(filename=ifile,embed=True)
+        elif itype == 'svg':
+            with file(ifile,"rb") as inf:
+                svgdata = inf.read()
+            img = display.SVG(data=svgdata)
+        else:
+            raise Exception("Invalid image type: {0}".format(itype))
+        if img:
+            display.display(img)
+
+    if not rescan:
+        for ext in EXTS:
+            ifile = basename + '.' + ext
+            if os.path.exists(ifile):
+                _display(ifile)
+                return
+                
+    ifile = basename + '.' + EXTS[-1]
+    cmd = "scan-to-file {0}".format(ifile)
+    os.system(cmd)
+    if os.path.exists(ifile):
+        _display(ifile)
+    else:
+        raise Exception("Unable to find image '{0}'; Tried these extensions: {1}".format(basename,EXTS))
+        
 
 _FLOATS = [float,np.float64,np.float32,np.float]
 
